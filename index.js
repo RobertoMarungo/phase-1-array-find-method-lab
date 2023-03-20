5f5e48271bda2dd7f90f73466f88c3dd1ef08cb6
Roberto Marungo <100833599+RobertoMarungo@users.noreply.github.com>
2023-03-20 22:40:07 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+const superbowlWin = (record) => {
+    const winResult = record.find((record) => record.result === "W");
+
+    if (winResult) {
+        return winResult.year;
+    } else {
+        return undefined;
+    }
+};
